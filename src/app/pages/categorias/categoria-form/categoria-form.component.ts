973b7355346ee0146e3aea5d6b2d884bd7aa4609
thiagooliveira01 <tuim_gm@hotmail.com>
2019-10-08 20:04:30 -0300

    Commit fim categoria

--- src/app/pages/categorias/categoria-form/categoria-form.component.ts
+++ src/app/pages/categorias/categoria-form/categoria-form.component.ts
@@ -1,15 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { Categoria } from "../shared/categoria.model";
+import { CategoriaService } from "../shared/categoria.service";
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
 
 @Component({
   selector: 'app-categoria-form',
   templateUrl: './categoria-form.component.html',
   styleUrls: ['./categoria-form.component.css']
 })
-export class CategoriaFormComponent implements OnInit {
+export class CategoriaFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoriaForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  categoria: Categoria = new Categoria();
+
+  constructor(
+    private categoriaService: CategoriaService, 
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoria();
+    this.loadCategoria();
+  }
+
+  ngAfterContentChecked(){
+    this.setPageTitle();
+  }
+
+  submitForm(){
+    this.submittingForm = true;
+
+    if (this.currentAction == 'new')
+      this.createCategoria();
+    else //edit
+      this.updateCategoria();
+  }
+
+  //metodos privados
+  private setCurrentAction(){
+    if(this.route.snapshot.url[0].path == "new")
+      this.currentAction = 'new'
+    else
+      this.currentAction = 'edit'
+  }
+
+  private buildCategoria(){
+    this.categoriaForm = this.formBuilder.group({
+      id: [null],
+      nome:[null, [Validators.required, Validators.minLength(2)]],
+      descricao:[null]
+    });
+  }
+
+  private loadCategoria(){
+    if(this.currentAction == 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoriaService.getById(+params.get("id")))
+      )
+      .subscribe(
+        (categoria) =>{
+          this.categoria = categoria
+          this.categoriaForm.patchValue(categoria) //carregando dados para formulario
+        },
+        (error) => alert('Ocorreu um erro no servidor.')
+      )
+    }
+  }
+
+  private setPageTitle(){
+    if (this.currentAction == 'new')  
+      this.pageTitle = 'Cadastro de nova categoria'
+    else{
+      const nomeCategoria = this.categoria.nome || "";
+      this.pageTitle = 'Editando categoria: '+ nomeCategoria;
+    }
+  }
+
+  private createCategoria(){
+    const categoria : Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
+
+    this.categoriaService.create(categoria).subscribe(
+      categoria => this.actionsForSuccess(categoria),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategoria(){
+    const categoria : Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
+    this.categoriaService.update(categoria).subscribe(
+      categoria => this.actionsForSuccess(categoria),
+      error => this.actionsForError(error)
+    );
+  }
+
+  private actionsForSuccess(categoria: Categoria){
+    toastr.success('Solicitação processada com sucesso!');
+
+    //volta para categorias e re-abre o formulario na rota de edit
+    this.router.navigateByUrl('categorias', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categorias', categoria.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+
+    this.submittingForm = false;
+
+    if(error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Tente mais tarde."];
   }
 
 }
--- src/app/pages/categorias/categorias.module.ts
+++ src/app/pages/categorias/categorias.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriasRoutingModule } from './categorias-routing.module';
 import { ListaCategoriaComponent } from './lista-categoria/lista-categoria.component';
 import { CategoriaFormComponent } from './categoria-form/categoria-form.component';
 
-
 @NgModule({
   declarations: [ListaCategoriaComponent, CategoriaFormComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     CategoriasRoutingModule
   ]
 })
