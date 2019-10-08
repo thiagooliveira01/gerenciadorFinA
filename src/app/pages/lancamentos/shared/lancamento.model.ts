8d02ef13ee53cd84b06c716c23d7cf5b61df79b2
thiagooliveira01 <tuim_gm@hotmail.com>
2019-10-08 20:16:10 -0300

    Commit Lancamento

---
+++ src/app/pages/lancamentos/shared/lancamento.model.ts
@@ -0,0 +1,24 @@
+import { Categoria } from '../../categorias/shared/categoria.model';
+
+export class Lancamento{
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public descricao?: string,
+        public tipo?: string,
+        public valor?: string,
+        public data?: string,
+        public pago?: boolean,
+        public categoriaId?: number,
+        public categoria?: Categoria
+    ){}
+
+    static types = {
+        despesa: 'Despesa',
+        receita: 'Receita'
+    };
+
+    get paidText(): string{
+        return this.pago ? 'Pago' : 'Pendente';
+    }
+}
--- src/app/pages/lancamentos/shared/lancamento.service.ts
+++ src/app/pages/lancamentos/shared/lancamento.service.ts
@@ -4,43 +4,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from "rxjs";
 import { map, catchError, flatMap } from "rxjs/operators";
 
-import { Categoria } from "./categoria.model";
+import { Lancamento } from "./lancamento.model";
+
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriaService {
+export class LancamentoService {
 
-  private apiPath : string = "api/categorias";
+  private apiPath : string = "api/lancamentos";
 
   constructor(private http: HttpClient) { }
 
-  getall(): Observable<Categoria[]>{
+  getall(): Observable<Lancamento[]>{
     return this.http.get(this.apiPath).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategorias)
+      map(this.jsonDataToLancamentos)
     )
   }
 
-  getById(id:number):Observable<Categoria>{
+  getById(id:number):Observable<Lancamento>{
     const url = `${this.apiPath}/${id}`;
     return this.http.get(url).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategoria)
+      map(this.jsonDataToLancamento)
     )
   }
 
-  create(categoria: Categoria): Observable<Categoria> {
-    return this.http.post(this.apiPath, categoria).pipe(
+  create(lancamento: Lancamento): Observable<Lancamento> {
+    return this.http.post(this.apiPath, lancamento).pipe(
       catchError(this.handleError),
-      map(this.jsonDataToCategoria)
+      map(this.jsonDataToLancamento)
     )
   }
 
-  update(categoria: Categoria): Observable<Categoria>{
-    const url = `${this.apiPath}/${categoria.id}`;
-    return this.http.put(url, categoria).pipe(
+  update(lancamento: Lancamento): Observable<Lancamento>{
+    const url = `${this.apiPath}/${lancamento.id}`;
+    return this.http.put(url, lancamento).pipe(
       catchError(this.handleError),
-      map(()=>categoria)
+      map(()=>lancamento)
     )
   }
 
@@ -54,14 +55,14 @@ export class CategoriaService {
   }
 
 //Medotodos privados
-  private jsonDataToCategorias(jsonData: any[]):Categoria[]{
-    const categorias: Categoria[] = [];
-    jsonData.forEach(element=>categorias.push(element as Categoria));
-    return categorias;
+  private jsonDataToLancamentos(jsonData: any[]):Lancamento[]{
+    const lancamentos: Lancamento[] = [];
+    jsonData.forEach(element=>lancamentos.push(element as Lancamento));
+    return lancamentos;
   }
 
-  private jsonDataToCategoria (jsonData: any): Categoria{
-    return jsonData as Categoria
+  private jsonDataToLancamento (jsonData: any): Lancamento{
+    return jsonData as Lancamento
   }
 
   private handleError(error:any): Observable<any>{
