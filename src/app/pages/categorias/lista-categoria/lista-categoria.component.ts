6fe763f0ea003f68966b81dfaca89d0543280c4a
thiagooliveira01 <tuim_gm@hotmail.com>
2019-10-07 22:45:22 -0300

    categoria-form

--- src/app/pages/categorias/lista-categoria/lista-categoria.component.ts
+++ src/app/pages/categorias/lista-categoria/lista-categoria.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Categoria } from "../shared/categoria.model";
+import { CategoriaService } from "../shared/categoria.service";
+
 @Component({
   selector: 'app-lista-categoria',
   templateUrl: './lista-categoria.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaCategoriaComponent implements OnInit {
 
-  constructor() { }
+  categorias : Categoria[] = [];
+
+  constructor(private categoriaService: CategoriaService ) { }
 
   ngOnInit() {
+    this.categoriaService.getall().subscribe(
+      categorias => this.categorias = categorias,
+      error => alert('Erro ao carregar a lista')
+    )
+  }
+
+  deleteCategoria(categoria){
+    const confirmaDelete = confirm('Deseja realmente excluir este item?');
+    if (confirmaDelete){
+    this.categoriaService.delete(categoria.id).subscribe(
+      ()=> this.categorias = this.categorias.filter(element => element != categoria),
+      ()=> alert('Erro ao tentar excluir')
+    )
+    }
   }
 
 }
