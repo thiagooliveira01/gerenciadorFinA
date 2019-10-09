de46912e8cd80325f771a96d497aa12fd849707c
thiagooliveira01 <tuim_gm@hotmail.com>
2019-10-09 20:52:47 -0300

    restruturacao

--- src/app/pages/categorias/shared/categoria.model.ts
+++ src/app/pages/categorias/shared/categoria.model.ts
@@ -1,7 +1,11 @@
-export class Categoria{
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+export class Categoria extends BaseResourceModel{
     constructor(
         public id?:number,
         public nome?: string,
         public descricao?: string
-    ){}
+    ){
+        super();
+    }
 }
--- src/app/pages/lancamentos/shared/lancamento.model.ts
+++ src/app/pages/lancamentos/shared/lancamento.model.ts
@@ -1,6 +1,7 @@
 import { Categoria } from '../../categorias/shared/categoria.model';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
 
-export class Lancamento{
+export class Lancamento extends BaseResourceModel{
     constructor(
         public id?: number,
         public nome?: string,
@@ -11,7 +12,9 @@ export class Lancamento{
         public pago?: boolean,
         public categoriaId?: number,
         public categoria?: Categoria
-    ){}
+    ){
+        super();
+    }
 
     static types = {
         despesa: 'Despesa',
