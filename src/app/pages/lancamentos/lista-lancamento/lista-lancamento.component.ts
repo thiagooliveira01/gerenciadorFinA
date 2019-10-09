366d727b47667c8be54384a2a7bf6a44989bf03d
thiagooliveira01 <tuim_gm@hotmail.com>
2019-10-09 20:08:53 -0300

    Commit lancamentos

--- src/app/pages/lancamentos/lista-lancamento/lista-lancamento.component.ts
+++ src/app/pages/lancamentos/lista-lancamento/lista-lancamento.component.ts
@@ -16,7 +16,7 @@ export class ListaLancamentoComponent implements OnInit {
 
   ngOnInit() {
     this.lancamentoService.getall().subscribe(
-      lancamentos => this.lancamentos = lancamentos,
+      lancamentos => this.lancamentos = lancamentos.sort((a, b)=> b.id - a.id ),
       error => alert('Erro ao carregar a lista')
     )
   }
