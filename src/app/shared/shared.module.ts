73ec064318b0284179fc3c49e294c7d3956ff52e
thiagooliveira01 <tuim_gm@hotmail.com>
2019-11-11 22:28:36 -0300

    otimizacao 6

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { BreadCrumbComponent } from './componentes/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports:[
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BreadCrumbComponent,
+    RouterModule
   ]
 })
 export class SharedModule { }
